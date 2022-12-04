6a75bf242c5920a04a2225c05721fe09aeb661db
J <jamscc22@gmail.com>
2022-12-04 00:20:46 -0500

    other team members - draft questions and summary

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const ManagerEmp = require('./lib/ManagerClass');
+const EngineerEmp = require('./lib/EngineerClass');
+const InternEmp = require('./lib/InternClass');
 const htmlFile = require('./src/createHTML');
 const inquirer = require('inquirer');
 
@@ -8,6 +11,16 @@ const questionsManager = [
     "Please enter the email address of the manager:",
     "Please enter the office number of the manager:",
     "Add an engineer or intern to the team profile?"
+];
+
+// questions - other team members
+const questionsOthers = [
+    "name",
+    "employee ID",
+    "email address",
+    "username (GitHub)",
+    "name of the school",
+    "Add others to the team profile?"
 ];
 
 // generate HTML
@@ -84,8 +97,116 @@ function managerInformation() {
                 </div>
             </div>`
 
-
+            // whether to add information about other team members to the team profile or generate team profile
+            if (dataManager.others != 'No') {
+                var employeePosition;
+                switch (dataManager.others) {
+                    case ('add Intern'):
+                        employeePosition = 'Intern';
+                        var additionalDataI = `${questionsOthers[4]} that the employee attends`;
+                        break;
+                    default:
+                        employeePosition = 'Engineer';
+                        var additionalDataE = `${questionsOthers[3]} of the Engineer`;
+                }
+                // information about the other team members
+                othersInformation(employeePosition, additionalDataI, additionalDataE, insertHTML);
+            } else {
+                writeHTML(insertHTML);
+            }
         })
 }
 
-managerInformation()
+// information about the other team members
+function othersInformation(employeePosition, additionalDataI, additionalDataE, insertHTML) {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "employeeName",
+            message: `Please enter the ${questionsOthers[0]} of the ${employeePosition}:`,
+            validate: valInformation
+        },
+
+        {
+            type: "input",
+            name: "employeeId",
+            message: `Please enter the ${questionsOthers[1]} of the ${employeePosition}:`,
+            validate: valInformation
+        },
+
+        {
+            type: "input",
+            name: "employeeEmail",
+            message: `Please enter the ${questionsOthers[2]} of the ${employeePosition}:`,
+            validate: valInformation
+        },
+
+        {
+            type: "input",
+            name: "employeeOtherData",
+            message: `Please enter the ${additionalDataI || additionalDataE}:`,
+            validate: valInformation
+        },
+
+        {
+            type: "list",
+            name: "addOthers",
+            message: `${questionsOthers[5]}`,
+            choices: ['No', 'add Engineer', 'add Intern']
+        },
+
+    ])
+        .then((dataOthers) => {
+            switch (employeePosition) {
+                case ('Intern'):
+                    // Bootstrap
+                    // object with empName, empId, empEmail, and school
+                    const internSummary = new InternEmp(dataOthers.employeeName, dataOthers.employeeId, dataOthers.employeeEmail, dataOthers.employeeOtherData);
+                    var summaryI = internSummary;
+                    var empPositionI = internSummary.getRole();
+                    var empSchoolData = `<p class="card-text">School Name: ${internSummary.school}</p>`;                    
+                    break;
+                default:
+                    // Bootstrap
+                    // object with empName, empId, empEmail, and github
+                    const engineerSummary = new EngineerEmp(dataOthers.employeeName, dataOthers.employeeId, dataOthers.employeeEmail, dataOthers.employeeOtherData);
+                    var summaryE = engineerSummary;
+                    var empPositionE = engineerSummary.getRole();
+                    var empGHData = `<p class="card-text">GitHub Link: <a href="https://github.com/${engineerSummary.github}" target="_blank">https://github.com/${engineerSummary.github}</a></p>`;
+            }
+            
+            const { empName, empId, empEmail } = summaryI || summaryE;
+            // Bootstrap
+            // other team member - summary
+            insertHTML += `
+            <div class="col-6 col-md-4">
+                <div class="card border-primary text-dark mb-3" style="max-width: 22rem;">
+                    <div class="card-header text-center">${empName}</div>
+                    <div class="card-body">
+                        <h6 class="card-title text-center">${empPositionI || empPositionE}</h6>
+                        <p class="card-text">Employee ID: ${empId}</p>
+                        <p class="card-text">Employee Email: <a href="mailto:${empEmail}">${empEmail}</a></p>
+                        ${empSchoolData || empGHData}
+                    </div>
+                </div>
+            </div>`
+
+            // whether to add information about other team members to the team profile or generate team profile
+            if (dataOthers.addOthers != 'No') {
+                switch (dataOthers.addOthers) {
+                    case ('add Intern'):
+                        employeePosition = 'Intern';
+                        var additionalDataI = `${questionsOthers[4]} that the employee attends`;
+                        break;
+                    default:
+                        employeePosition = 'Engineer';
+                        var additionalDataE = `${questionsOthers[3]} of the Engineer`;                
+                }
+                othersInformation(employeePosition, additionalDataI, additionalDataE, insertHTML);
+            } else {
+                writeHTML(insertHTML);
+            }
+        })
+}
+
+managerInformation();
